264bf403bd0638982eb65b3eac93ea1375756ec2
Anke <a.stein@posteo.de>
2021-09-09 08:48:10 +0200

    create gitignore file

---
+++ src/CalculatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd(){
+        // GIVE
+        int a = 1;
+        int b = 2;
+
+        // WHEN
+        int result = Calculator.add(a, b);
+
+        // EXPECT / THEN
+        Assertions.assertEquals(3, result);
+    }
+}
